12b1503a4045dadc66973fec061653d7aaa1065f
Björn <bjorn@hfinfo.se>
2016-02-17 14:08:54 +0100

    tried to add items to spinners

--- GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/FirstUsageActivity.java
+++ GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/FirstUsageActivity.java
@@ -2,6 +2,12 @@ package se.chalmers.phrasebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstUsageActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class FirstUsageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_usage);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.language_array,
+                android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+    }
+     //Test class to develop buttons
+    public void sendMessage(View view) {
+        System.out.println("START");
     }
 }

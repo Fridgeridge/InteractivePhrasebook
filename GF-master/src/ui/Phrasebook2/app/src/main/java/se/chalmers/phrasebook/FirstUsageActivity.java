547bbba7928040b64305582c03d44e492ed35b13
Björn <bjorn@hfinfo.se>
2016-02-17 13:28:10 +0100

    basic first usage activity, sligth upgrade to the colors.xml

---
+++ GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/FirstUsageActivity.java
@@ -0,0 +1,13 @@
+package se.chalmers.phrasebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstUsageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_usage);
+    }
+}

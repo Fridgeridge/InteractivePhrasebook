d7a3daa98c6fdc9d32205423a92f7ed02b96c426
Fridgeridge <d.michaelsson@gmail.com>
2016-03-03 16:52:58 +0100

    Added lang Enum and work on translator

--- GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/backend/XMLParser.java
+++ GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/backend/XMLParser.java
@@ -89,7 +89,7 @@ public class XMLParser {
         if (nl == null || nl.getLength() < 1)
             return null;
         int length = nl.getLength();
-        String syntax = "", desc = "", option = "";
+        String syntax = "", desc = "", question="", option ;
         for (int i = 0; i < length; i++) {
             if (nl.item(i) != null && (nl.item(i).getNodeType() == Node.ELEMENT_NODE) && nl.item(i).getAttributes() != null) {
                 NamedNodeMap attributes = nl.item(i).getAttributes();
@@ -98,21 +98,34 @@ public class XMLParser {
                     syntax = attributes.getNamedItem("syntax").getNodeValue();
 
                 }
+
                 if (attributes.getNamedItem("desc") != null) {
                     desc = attributes.getNamedItem("desc").getNodeValue();
                 }
+
+
+                if (attributes.getNamedItem("question") != null) {
+                    question = attributes.getNamedItem("childdesc").getNodeValue();
+                }
+
+
                 if (attributes.getNamedItem("child") != null) {
                     option = attributes.getNamedItem("child").getNodeValue();
 
-                    NodeList list = jumpToChild(document,option);
-                    tree.addChild(parent, constructSentence(list,tree,parent));
-
+                    NodeList list = jumpToChild(document, option);
+                    tree.addChild(parent, constructSentence(list, tree, parent));
                 }
-                SyntaxTree.Node child = new SyntaxTree.Node(syntax);
-                child.setDescription(desc);
-                tree.addChild(parent,child);
+
+                if (!syntax.isEmpty()) {
+                    SyntaxTree.Node child = new SyntaxTree.Node(syntax);
+
+                    if (!question.isEmpty()) parent.setChildDescription(question);
+                    if (!desc.isEmpty()) child.setDataDescription(desc);
+
+                    tree.addChild(parent, child);//Adds the current node to the parent
+                    tree.addChild(child,constructSentence(nl.item(i).getChildNodes(), tree, child));//recursively adds grandchild to child, until nodelist is empty or null
+                }
             }
-            constructSentence(nl.item(i).getChildNodes(),tree, parent);
         }
         return parent;
     }

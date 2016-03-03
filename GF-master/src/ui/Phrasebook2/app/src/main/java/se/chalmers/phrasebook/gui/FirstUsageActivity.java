398cdc89a6c6a32605256abfd03e71e2399b407c
MatildaHorppu <matilda@dhcp-178181.eduroam.chalmers.se>
2016-03-03 16:05:36 +0100

    Adding values to spinner in java instead of from xml

--- GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/FirstUsageActivity.java
+++ GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/FirstUsageActivity.java
@@ -12,24 +12,37 @@ import org.w3c.dom.Document;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import se.chalmers.phrasebook.R;
+import se.chalmers.phrasebook.backend.Language;
+import se.chalmers.phrasebook.backend.Model;
 import se.chalmers.phrasebook.backend.SyntaxTree;
 import se.chalmers.phrasebook.backend.XMLParser;
 
 public class FirstUsageActivity extends AppCompatActivity {
     private PGF pgf;
 
+    private List<String> languages;
+
+    private Model model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        model = new Model();
+
         setContentView(R.layout.activity_first_usage);
 
         Spinner originSpinner = (Spinner) findViewById(R.id.origin_spinner);
         Spinner targetSpinner = (Spinner) findViewById(R.id.target_spinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.language_array,
-                android.R.layout.simple_spinner_item);
+        languages = new ArrayList<String>();
+        initLanguages();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, languages, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         originSpinner.setAdapter(adapter);
         targetSpinner.setAdapter(adapter);
@@ -51,5 +64,16 @@ public class FirstUsageActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         Intent intent = new Intent(this, HomeScreenActivity.class);
         startActivity(intent);
+    }
+
+
+    //Generate strings for spinners
+    private void initLanguages(){
+        List temp = model.getLanguages();
+
+        for(int i = 0; i < temp.size(); i++){
+            languages.add(temp.get(i).getClass().getName());
+        }
+
     }
 }

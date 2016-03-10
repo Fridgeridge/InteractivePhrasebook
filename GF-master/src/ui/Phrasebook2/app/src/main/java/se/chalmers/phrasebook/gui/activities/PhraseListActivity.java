d36192d9170ded55613553a6ba8c69f6276cb296
MatildaHorppu <matilda.horppu@gmail.com>
2016-03-10 20:13:47 +0100

    can now change to translatorActivity

--- GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/activities/PhraseListActivity.java
+++ GF-master/src/ui/Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/activities/PhraseListActivity.java
@@ -1,8 +1,12 @@
 package se.chalmers.phrasebook.gui.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,6 +19,7 @@ public class PhraseListActivity extends AppCompatActivity {
 
     private Model model;
     private ArrayList<String> phrases;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,8 @@ public class PhraseListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phrase_list);
         initializeList(model.getCurrentPhrasebook());
 
+        context = this;
+
     }
 
 
@@ -33,8 +40,17 @@ public class PhraseListActivity extends AppCompatActivity {
 
         ArrayAdapter<CharSequence> phraseAdapter = ArrayAdapter.createFromResource(this, R.array.phrases,
                 android.R.layout.simple_list_item_1);
+
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(phraseAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(context, TranslatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     //correct function

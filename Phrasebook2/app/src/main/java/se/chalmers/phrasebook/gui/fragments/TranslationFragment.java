7fde192dbfb5823ebab44ab0259f4de53423d990
Fridgeridge <d.michaelsson@gmail.com>
2016-03-16 16:21:47 +0100

    Translation to translationfragment

--- Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/fragments/TranslationFragment.java
+++ Phrasebook2/app/src/main/java/se/chalmers/phrasebook/gui/fragments/TranslationFragment.java
@@ -5,25 +5,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import se.chalmers.phrasebook.R;
+import se.chalmers.phrasebook.backend.Model;
 
 /**
  * Created by matilda on 10/03/16.
  */
 public class TranslationFragment extends Fragment {
 
+    private View translateView;
+    private Model model;
+    private TextView origin,target;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        model = Model.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_translation, container, false);
+
+        translateView = inflater.inflate(R.layout.fragment_translation, container, false);
+
+        origin = (TextView) translateView.findViewById(R.id.origin_phrase);
+        target = (TextView) translateView.findViewById(R.id.target_phrase);
+
+        return translateView;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String originTranslation = model.translateToOrigin();
+        String targetTranslation = model.translateToTarget();
+        origin.setText(originTranslation);
+        target.setText(targetTranslation);
+
+    }
+
+
 
 }

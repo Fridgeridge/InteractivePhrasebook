09ea1b26b3aab12baa7a84e3ea7f92f21b274869
Fridgeridge <d.michaelsson@gmail.com>
2016-04-01 11:38:01 +0200

    Smallfixes

--- Phrasebook2/app/src/main/java/se/chalmers/phrasebook/backend/Langs.java
+++ Phrasebook2/app/src/main/java/se/chalmers/phrasebook/backend/Langs.java
@@ -3,6 +3,7 @@ package se.chalmers.phrasebook.backend;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -10,41 +11,42 @@ import java.util.Map;
  */
 public enum Langs {
 
-    RUSSIAN("ру́сский","Russian","PhrasebookRus"),
-    BULGARIAN("български","Bulgarian","PhrasebookBul"),
-    HINDU("हिन्दी","Hindi","PhrasebookHin"),
-    CATALAN("Català","Catalan","PhrasebookCat"),
-    THAI("","Thai","PhrasebookTha"),
-    PERSIAN("","Persian","PhrasebookPes"),
-    FINNISH("","Finnish","PhrasebookFin"),
-    JAPANESE("","Japanese","PhrasebookJpn"),
-    POLISH("","Polish","PhrasebookPol"),
-    SWEDISH("Svenska","Swedish","PhrasebookSwe"),
-    URDU("","Urdu","PhrasebookUrd"),
-    ROMANIAN("","Romanian","PhrasebookRon"),
-    CHINESE("","Chinese", "PhrasebookChi"),
-    ITALIAN("","Italian","PhrasebookIta"),
-    DANISH("","Danish","PhrasebookDan"),
-    SPANISH("","Spanish","PhrasebookSpa"),
-    LATVIAN("","Latvian","PhrasebookLav"),
-    ENGLISH("English","English","PhrasebookEng"),
-    NORWEGIAN("","Norwegian","PhrasebookNor"),
-    GERMAN("","German","PhrasebookGer"),
-    DUTCH("","Dutch","PhrasebookDut"),
-    FRENCH("","French","PhrasebookFre");
+    RUSSIAN("ру́сский","Russian","PhrasebookRus", null),
+    BULGARIAN("български","Bulgarian","PhrasebookBul",null),
+    HINDU("हिन्दी","Hindi","PhrasebookHin",null),
+    CATALAN("Català","Catalan","PhrasebookCat",null),
+    THAI("","Thai","PhrasebookTha",null),
+    PERSIAN("","Persian","PhrasebookPes",null),
+    FINNISH("","Finnish","PhrasebookFin",null),
+    JAPANESE("","Japanese","PhrasebookJpn",Locale.JAPANESE),
+    POLISH("","Polish","PhrasebookPol",null),
+    SWEDISH("Svenska","Swedish","PhrasebookSwe",null),
+    URDU("","Urdu","PhrasebookUrd",null),
+    ROMANIAN("","Romanian","PhrasebookRon",null),
+    CHINESE("","Chinese", "PhrasebookChi",Locale.CHINESE),
+    ITALIAN("","Italian","PhrasebookIta",Locale.ITALIAN),
+    DANISH("","Danish","PhrasebookDan",null),
+    SPANISH("","Spanish","PhrasebookSpa",null),
+    LATVIAN("","Latvian","PhrasebookLav",null),
+    ENGLISH("English","English","PhrasebookEng",Locale.ENGLISH),
+    NORWEGIAN("","Norwegian","PhrasebookNor",null),
+    GERMAN("","German","PhrasebookGer",null),
+    DUTCH("","Dutch","PhrasebookDut",null),
+    FRENCH("","French","PhrasebookFre",Locale.FRENCH);
 
 
     private final String language;
     private final String key;
     private final String nativeSpelling;
-
+    private final Locale ttsLang;
 
     private static final Map<String, String> langMap = Collections.unmodifiableMap(initializeMap());
 
-    Langs(String spelling, String language, String key){
+    Langs(String spelling, String language, String key, Locale locale){
         this.nativeSpelling = spelling;
         this.language = language;
         this.key = key;
+        this.ttsLang = locale;
     }
 
     public String getKey(){
@@ -53,6 +55,10 @@ public enum Langs {
 
     public String getLang(){
         return this.language;
+    }
+
+    public Locale getTTS(){
+        return this.ttsLang;
     }
 
     private static Map<String, String> initializeMap(){
@@ -84,4 +90,5 @@ public enum Langs {
         }
         return null;
     }
+
 }
